0723e1697b406ec1c2c75293db909d8ca353badc
andylockran <andy@zrmt.com>
2021-05-10 17:56:11 +0100

    Linted js

--- functions/__tests__/draft4.spec.js
+++ functions/__tests__/draft4.spec.js
@@ -1,48 +1,48 @@
 import draft4 from "../draft4";
 
 describe("JSONSchema Draft-4", () => {
-    test("A valid JSON Schema should pass with no errors", () => {
-        const input = {
-                "type": "object",
-                "properties": {
-                  "name": {
-                    "type": "string"
-                  },
-                  "petType": {
-                    "type": "string"
-                  }
-                },
-                "required": [
-                  "name",
-                  "petType"
-                ]
-              }
+  test("A valid JSON Schema should pass with no errors", () => {
+    const input = {
+      "type": "object",
+      "properties": {
+        "name": {
+          "type": "string"
+        },
+        "petType": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "name",
+        "petType"
+      ]
+    }
     const output = undefined;
     expect(draft4(input)).toEqual(output);
-    });
+  });
 
-    test("An invalid JSON Schema should fail with an errors", () => {
-      const input = {
-              "type": "object",
-              "properties": {
-                "name": {
-                  "type": "string",
-                  "format": "string"
-                },
-                "petType": {
-                  "type": "string"
-                }
-              },
-              "required": [
-                "name",
-                "petType"
-              ]
-            }
-  const output = [
-    {
-      message: 'Not valid JSONSchema4: Error: unknown format "string" is used in schema at path "#/properties/name"'
+  test("An invalid JSON Schema should fail with an errors", () => {
+    const input = {
+      "type": "object",
+      "properties": {
+        "name": {
+          "type": "string",
+          "format": "string"
+        },
+        "petType": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "name",
+        "petType"
+      ]
     }
-  ];
-  expect(draft4(input)).toEqual(output);
+    const output = [
+      {
+        message: 'Not valid JSONSchema4: Error: unknown format "string" is used in schema at path "#/properties/name"'
+      }
+    ];
+    expect(draft4(input)).toEqual(output);
   });
-  });
+});
