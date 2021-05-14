24fca3d6a7f2d20ac69f39d0176838886019f7ce
Andy Loughran <andy@lockran.com>
2021-05-14 22:58:54 +0100

    Tidying up tests

--- src/__tests__/aws_important_notes.spec.js
+++ src/__tests__/aws_important_notes.spec.js
@@ -1,13 +1,23 @@
-const { Spectral } = require("@stoplight/spectral");
-const ruleset = require("../aws_important_notes.yml")
+const { Spectral, isOpenApiv2, isOpenApiv3 } = require('@stoplight/spectral');
+const { join } = require('path');
+var yaml = require('js-yaml');
+var fs = require('fs');
+const petstore = fs.readFileSync(join(__dirname, "../../examples/petstore_aws.yaml"), { encoding: "utf-8"});
 
 const spectral = new Spectral();
-spectral.setRuleset(ruleset);
+spectral.registerFormat('oas2', isOpenApiv2);
+spectral.registerFormat('oas3', isOpenApiv3);
 
 describe("AWS Important Notes", () => {
     test("The example in the examples folder is a valid document", () => {
-
-    })  ;  
+        const input = petstore;
+        spectral
+        .loadRuleset(join(__dirname, "../../aws_important_notes.yml"))
+        .then(() => spectral.run(input))
+        .then(results => {
+            expect(results).toEqual([]);
+        });
+    });  
     test("For input parameters, only the following attributes are supported.", () => {
 
     })
