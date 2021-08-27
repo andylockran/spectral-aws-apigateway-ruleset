a4888367ffd69106db21df7c8f9b33c43eaa0bde
andylockran <andy@zrmt.com>
2021-08-27 15:52:40 +0100

    Updated draft4

--- src/__tests__/draft4.spec.js
+++ src/__tests__/draft4.spec.js
@@ -1,77 +1,10 @@
 import draft4 from "../draft4";
-// https://datatracker.ietf.org/doc/html/draft-fge-json-schema-validation-00#section-4.1
-// Including additional formats as described above ^^
-describe("JSONSchema Draft-4", () => {
-  test("A valid JSON Schema should pass with no errors", () => {
-    const input = {
-      "type": "object",
-      "properties": {
-        "name": {
-          "type": "string"
-        },
-        "petType": {
-          "type": "string"
-        }
-      },
-      "required": [
-        "name",
-        "petType"
-      ]
-    }
-    const output = undefined;
-    expect(draft4(input)).toEqual(output);
-  });
-
-  test("An invalid JSON Schema should fail with an error", () => {
-    const input = {
-      "type": "object",
-      "properties": {
-        "name": {
-          "type": "string",
-          "format": "string"
-        },
-        "petType": {
-          "type": "string"
-        }
-      },
-      "required": [
-        "name",
-        "petType"
-      ]
-    }
-    const output = [
-      {
-        message: 'Not valid JSONSchema4: Error: unknown format "string" ignored in schema at path "#/properties/name"'
-      }
-    ];
-    expect(draft4(input)).toEqual(output);
-  });
-
-  test("format date isn't valid in draft-4", () => {
-    const input = {
-      "type": "object",
-      "properties": {
-        "isadate": {
-          "type": "string",
-          "format": "date"
-        }
-      }
-    }
-    const output = [{"message": "Not valid JSONSchema4: Error: unknown format \"date\" ignored in schema at path \"#/properties/isadate\""}];
-    expect(draft4(input)).toEqual(output);
-  });
-
-  test("format date-time is valid in draft-4", () => {
-    const input = {
-      "type": "object",
-      "properties": {
-        "isadate": {
-          "type": "string",
-          "format": "date-time"
-        }
-      }
-    }
-    const output = undefined;
-    expect(draft4(input)).toEqual(output);
-  });
-});
+const yaml = require('js-yaml');
+const fs   = require('fs')
+describe("JSONSchema Compile All", () => {
+    test("A schema should compile across all $refs", () => {
+      const input = yaml.load(fs.readFileSync('examples/petstore_aws.yaml', 'utf8'));
+      const output = undefined;
+      expect(draft4(input)).toEqual(output);
+    })
+})
--- src/draft4.mjs
+++ src/draft4.mjs
@@ -1,7 +1,9 @@
 import Ajv from "ajv-draft-04"
 import addFormats from "ajv-formats"
 
-const ajv = new Ajv();
+const ajv = new Ajv({
+    "strict": true,
+});
 addFormats(ajv, ["date-time","email","hostname","ipv4","ipv6","uri"])
     
 ajv.addKeyword("example"); // Added 'example' keyword as picked up by `aws-example-tag` rule.
@@ -12,12 +14,22 @@ ajv.addKeyword("example"); // Added 'example' keyword as picked up by `aws-examp
 
 module.exports = targetVal => {
     try {
-        ajv.compile(targetVal);
+        const schemaList = targetVal.components.schemas
+        for (const property in schemaList) {
+            if (Object.prototype.hasOwnProperty.call(schemaList, property)) {
+                const schema_model = schemaList[property]
+                schema_model.id = `https://example.com/schema.json#/components/schemas/${property}`
+                ajv.addSchema(schemaList[property])
+            }
+        }
+        for (const schema in schemaList) {
+            let test = ajv.compile(schemaList[schema])
+        }
     }
     catch (error) {
         return [ 
             { 
-                message: `Not valid JSONSchema4: ${error}`,
+                message: `Invalid schema: ${error}`,
             }
         ]
     }
