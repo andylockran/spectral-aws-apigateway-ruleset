07f11fcfa685e6b52924155d3eb5c45eb5f2a910
andylockran <andy@zrmt.com>
2021-08-27 17:11:21 +0100

    Cleaned up draft4 to better validate schemas

--- src/draft4.mjs
+++ src/draft4.mjs
@@ -12,25 +12,28 @@ ajv.addKeyword("example"); // Added 'example' keyword as picked up by `aws-examp
     
 // };
 
-module.exports = targetVal => {
-    try {
-        const schemaList = targetVal.components.schemas
-        for (const property in schemaList) {
-            if (Object.prototype.hasOwnProperty.call(schemaList, property)) {
-                const schema_model = schemaList[property]
-                schema_model.id = `https://example.com/schema.json#/components/schemas/${property}`
-                ajv.addSchema(schemaList[property])
-            }
-        }
-        for (const schema in schemaList) {
-            let test = ajv.compile(schemaList[schema])
+module.exports = (targetVal, _opts, context) => {
+    let openapi = JSON.parse(JSON.stringify(targetVal))
+    const results = []
+
+    const schemaList = openapi.components.schemas
+    for (const property in schemaList) {
+        if (Object.prototype.hasOwnProperty.call(schemaList, property)) {
+            const schema_model = schemaList[property]
+            schema_model.id = `#/components/schemas/${property}`
+            ajv.addSchema(schemaList[property])
         }
     }
-    catch (error) {
-        return [ 
-            { 
-                message: `Invalid schema: ${error}`,
-            }
-        ]
+    for (const property in schemaList) {
+        try {
+            ajv.compile(schemaList[property])
+        }
+        catch (error) {
+            results.push({
+                "message": `${error}`,
+                "path": ['components', 'schemas', property]
+            })
+        }
     }
+    return results
 }
